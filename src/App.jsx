86e1f8a69b33471c6a8141853435e66e37d5a37a
Louis Cox <louiscox951@gmail.com>
2025-08-05 22:32:29 +0100

    Orders & Analytics Pages
    Created Orders & Analyics Pages and Components, including multiple charts and new data cards for the analytics page.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
 import UsersPage from "./pages/UsersPage";
 import SalesPage from "./pages/SalesPage";
+import OrdersPage from "./pages/OrdersPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
 
 import Sidebar from "./components/Sidebar";
 
@@ -22,6 +24,8 @@ function App() {
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/sales" element={<SalesPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
       </Routes>
     </div>
   );
