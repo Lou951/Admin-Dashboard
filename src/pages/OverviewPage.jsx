242e68f605f06a010fb8a54ef3c62b40bcbcfd3e
Louis Cox <louiscox951@gmail.com>
2025-08-05 14:14:26 +0100

    Overview Charts completed.

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -5,6 +5,8 @@ import StatCard from "../components/common/StatCard";
 import { motion } from "framer-motion";
 import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
 import SalesOverviewChart from "../components/overview/SalesOverviewChart";
+import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
+import SalesChannelChart from "../components/overview/SalesChannelChart";
 
 const OverviewPage = () => {
   return (
@@ -44,8 +46,10 @@ const OverviewPage = () => {
             color="#10b981"
           />
           {/* CHARTS */}
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
             <SalesOverviewChart />
+            <CategoryDistributionChart />
+            <SalesChannelChart />
           </div>
         </motion.div>
       </main>
